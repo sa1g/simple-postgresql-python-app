cb336ba440c270224442150d1f382165d3f5f7e7
Ettore Saggiorato <ettoreitaut4.1@gmail.com>
2020-12-08 16:04:39 +0100

    dec 8, 4:04 code bc - sould be the near final version

--- assignment3-LOCAL.py
+++ assignment3-LOCAL.py
@@ -3,32 +3,16 @@ import uuid
 import psycopg2
 import string
 import sys
-import psycopg2.extras 
+import psycopg2.extras
 from functools import wraps
 from time import time
+import logging
+logger = logging.getLogger(__name__)
 
-
-#public variables
+# global variables
+# G: not a good idea ever
 N = 1000000
 
-#useful functions
-def get_random_numbers_in_list():
-    """generates N unique random number in N range"""
-    #used in step 3
-    a = random.sample([x for x in range(N) if x != 185], N-1)
-    a.append(185)
-    #print(a)
-    return a
-
-def get_age():
-    """generates N random ages"""
-    #used in Step 3
-    a = []
-    for i in range(N):
-        x = random.randint(18, 70)
-        a.append(x)
-    #print(a)
-    return a
 
 def measure(n_step):
     def wrapper(func):
@@ -40,137 +24,129 @@ def measure(n_step):
                 return func(*args, **kwargs)
             finally:
                 end_ = time() - start
-                print(f"Step {n_step} needs {round(end_*(10**9))  if end_ > 0 else 0} ns")     
+                # use logging for this
+                print(
+                    f"Step {n_step} needs {round(end_*(10**9))  if end_ > 0 else 0} ns")
         return _time_it
     return wrapper
 
-def get_random_string_in_list(length):
-    """return a list of random STRING of length elements """
-    a = []
-    for i in range(N):
-        a.append(''.join(random.choice(string.ascii_uppercase) for i in range(length)))
-    #print(a)
+
+def connect():
+    """ Connect to the PostgreSQL database server, returns connectoin and cursor """
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(
+            database="assignment3", user="assignment3", host="localhost", password="assignment3")
+
+        cur = conn.cursor()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    return(conn, cur)
+# useful functions
+
+
+def get_random_numbers_in_list():
+    """generates N unique random number in N range, returns a list"""
+    # used in step 3
+    # TODO convert to generator (see below yield)
+    a = random.sample([x for x in range(N) if x != 185], N-1)
+    a.append(185)
+
     return a
 
-def get_random_bid_in_list():
-    #not gonna use it: too slow
-    """return a list of cryptographically okay random STRING of 25 elements """
-    a = []
+
+def get_age():
+    """generates N random ages, returns a list"""
+    # used in Step 3
     for i in range(N):
-        a.append(''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(25)))
-    #print(a)
-    return a
+        # hard coded values should go all togheter in a config section
+        x = random.randint(18, 70)
+        yield x
+
+
+def get_random_string_in_list(length):
+    """return a list of random STRING of length elements """
+    for i in range(N):
+        yield (''.join(random.choice(string.ascii_uppercase) for i in range(length)))
+
 
 def get_random_uuid(string_length=10):
     """Returns a random string of length string_length."""
-    random = str(uuid.uuid4()) # Convert UUID format to a Python string.
-    random = random.upper() # Make all characters uppercase.
-    random = random.replace("-","") # Remove the UUID '-'.
-    return random[0:string_length] # Return the random string.
+    random = str(uuid.uuid4())          # Convert UUID format to a Python string.
+    random = random.upper()             # Make all characters uppercase.
+    random = random.replace("-", "")    # Remove the UUID '-'.
+    return random[0:string_length]      # Return the random string.
+
 
 def uuid_list(string_length):
-    """list "my_random_string for N"""
-    a = []
+    """list of get_random_uuid, length: N"""
     for i in range(N):
-        a.append(get_random_uuid(string_length))
-    return a
+        yield get_random_uuid(string_length)
+
 
 def sailor_list():
     #id:        sequenziali
-    #name(50):  get_random_string_in_list
+    # name(50):  get_random_string_in_list
     #address:   ""
     #age:       get_age
     #captain:   get_random_number_in_list
-    #~8.5s creating "data" with i7 laptop
+    # ~8.5s creating "data" with i7 laptop
+
     name = get_random_string_in_list(10)
     address = get_random_string_in_list(10)
     age = get_age()
     captain = get_random_numbers_in_list()
     data = []
-    for i in range(N):
-        data.append((i, name[i], address[i], age[i], captain[i]))
-        
+
+    # the loop befor was C style - slower than this one
+    # with this one I can go back to the functions and
+    # make them return generators instead of list,
+    # this avoids storing data in memory, which is uselss for this task
+    # zip stop at the shortest list
+    for i, (nm, addr, ag, cp) in enumerate(zip(name, address, age, captain)):
+        data.append((i, nm, addr, ag, cp))
+
     return data
 
-#!new function
+
 def get_random_int_in_list():
     """return a list of random int between 0 and N"""
-    a = []
+
     for i in range(N):
-        a.append(random.randint(0, N-1))
-    return a
-#!new function
+        yield (random.randint(0, N-1))
+
+
 def boat_list():
-    """return a list of "boat"'s tuples, we know that "captain"'s range is between 0 and N (because of how the DB is 
+    """return a list of "boat"'s tuples, we know that "captain"'s range is between 0 and N (because of how the DB is
     developed, so we can generate captain directly in that range w/o needing to query that column from the DB) """
     bid = uuid_list(15)
     bname = get_random_string_in_list(20)
     size = get_random_string_in_list(20)
     captain = get_random_int_in_list()
 
-    a = []
-    for i in range(N):
-        a.append((bid[i], bname[i], size[i], captain[i]))
-    return a
-#function here for test purpose only
-#@measure
-def connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-    # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        
-        conn = psycopg2.connect(database="assignment3", user="assignment3", host="localhost", password="assignment3")
-    # create a cursor
-        cur = conn.cursor()
-        
-	# execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
+    for a, b, c, d in zip(bid, bname, size, captain):
+        yield (a, b, c, d)
 
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-       
-	# close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
 
-#Step 1 done
+# Step 1 done
+
+
 @measure(1)
-def drop_tables():
+def drop_tables(cur):
     """ drop tables if they already exist in the PostgreSQL database"""
-    conn = None
+    cur.execute(
+        """DROP TABLE IF EXISTS "Sailor" CASCADE; DROP TABLE IF EXISTS "Boat";""")
 
-    try:
-        #connect to the PostgreSQL server
-        conn = psycopg2.connect(database="assignment3", user="assignment3", host="localhost", password="assignment3")
-        cur = conn.cursor()
-        #drop tables one by one
-        
-        cur.execute("""DROP TABLE IF EXISTS "Sailor" CASCADE; DROP TABLE IF EXISTS "Boat";""")
-        
-        #close communication w/PSQL server
-        cur.close()
-        #commit the changes
-        conn.commit()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
 
-#Step 2 done
+# Step 2 done
+
+
 @measure(2)
-def create_tables():
+def create_tables(cur):
     """create tables in the PSQL database"""
-   
+
     commands = (
         """ CREATE TABLE "Sailor"(
             "id"      INT PRIMARY KEY,
@@ -186,244 +162,135 @@ def create_tables():
          "captain" INT NOT NULL REFERENCES "Sailor"("id") );"""
     )
 
-    conn = None
+    for command in commands:
+        cur.execute(command)
 
-    try:
-        #connect to the PGSQL Server
-        conn = psycopg2.connect(database="assignment3", user="assignment3", host="localhost", password="assignment3")
-        cur = conn.cursor()
-        #create tables one by one
-        
 
-        for command in commands:
-            #check for safety
-            #print("Excecuted command " + command)
-            cur.execute(command)
-        
-        #close comm with the PGSQL Server
-        cur.close()
-        #commit the changes
-        conn.commit()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+# Step 3 done
 
-#Step 3 done 
+
 @measure(3)
-def insert_tuple_Sailor():
+def insert_tuple_Sailor(cur):
     """generates 1M tuples of Sailor."""
     sql = """INSERT INTO "Sailor" (id, name, address, age, level) VALUES %s;"""
-    #ext 16 minutes at UNI, 20s in localhost
+    # ext 16 minutes at UNI, 20s in localhost
     argslist = sailor_list()
-    conn = None
-    try:  
-        #connect to the PGSQL Server
-        conn = psycopg2.connect(database="assignment3", user="assignment3", host="localhost", password="assignment3")
-        cur = conn.cursor()
-       
-        psycopg2.extras.execute_values(cur, sql, argslist)
-        #close comm with the PGSQL Server
-        cur.close()
-        #commit the changes
-        conn.commit()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print("shit")
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
 
-#Step 4 done
+    psycopg2.extras.execute_values(cur, sql, argslist)
+
+# Step 4 done
+
+
 @measure(4)
-def insert_tuple_Boat():
+def insert_tuple_Boat(cur):
     """generates 1M tuples of Boat, fk captain from Sailor"""
     sql = """INSERT INTO "Boat" (bid, bname, size, captain) VALUES %s;"""
-    argslist = boat_list()
-    conn = None
-    try:  
-        #connect to the PGSQL Server
-        conn = psycopg2.connect(database="assignment3", user="assignment3", host="localhost", password="assignment3")
-        cur = conn.cursor()
-       
-        psycopg2.extras.execute_values(cur, sql, argslist)
-        #close comm with the PGSQL Server
-        cur.close()
-        #commit the changes
-        conn.commit()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print("shit")
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-    
-    #TODO remove this function when finished
-def prova():
-    
-    conn = None
-    try:  
-        #connect to the PGSQL Server
-        conn = psycopg2.connect(database="assignment3", user="assignment3", host="localhost", password="assignment3")
-        cur = conn.cursor()
-        cur.execute("""SELECT * FROM "Sailor" ORDER BY 1""")
-        print(cur.rowcount)
-        a = cur.fetchall()
-        for x in a:
-            print(x)
-        #close comm with the PGSQL Server
-        cur.close()
-        #commit the changes
-        #conn.commit()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print("shit")
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+    argslist = list(boat_list())
 
-#!Step 5 ERROR IN OUTPUT, rest of the code is fine
+    psycopg2.extras.execute_values(cur, sql, argslist)
+
+
+# Step 5 done
+
+
 @measure(5)
-def id_sailor_stderr():
+def id_sailor_stderr5(cur):
     """retrieves from the database and prints to stderr all the id of the 1M Sailors """
-    conn = None
-    try:  
-        #connect to the PGSQL Server
-        conn = psycopg2.connect(database="assignment3", user="assignment3", host="localhost", password="assignment3")
-        cur = conn.cursor()
-        
-        cur.execute("""SELECT "id" FROM "Sailor" """)
-        
-        a = cur.fetchall()
-        
-        for x in a:
-            #error here.
-            #!sys.stderr.write(str(x))
-            #qui solo per far eseguire il tutto
-            print(x[0])
-        
-        #close comm with the PGSQL Server
-        cur.close()
-        #commit the changes
-        #conn.commit()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print("shit")
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+    cur.execute("""SELECT "id" FROM "Sailor" """)
 
-#Step 6 done
+    a = cur.fetchall()
+    for x in a:
+        print(x[0], file=sys.stderr)
+
+
+# Step 6 done
+
+
 @measure(6)
-def update_tuples_185():
+def update_tuples_185(cur):
     """Updates tuples where level = 185 ==> level = 200 and prints them to stderr """
-    conn = None
-    try:  
-        #connect to the PGSQL Server
-        conn = psycopg2.connect(database="assignment3", user="assignment3", host="localhost", password="assignment3")
-        cur = conn.cursor()
-        
-        cur.execute("""UPDATE "Sailor" SET "level" = 200 WHERE "level" = 185 """)
-        
-        #close comm with the PGSQL Server
-        cur.close()
-        #commit the changes
-        #conn.commit()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print("shit")
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
 
-#Step 7 done
+    cur.execute("""UPDATE "Sailor" SET "level" = 200 WHERE "level" = 185 """)
+
+
+# Step 7 done
+
+
 @measure(7)
-def print_tuples_200():
+def print_tuples_200(cur):
     """selects from the table sailor and prints to stderr the id and address fo the sailors with level = 200 """
-    conn = None
-    try:  
-        #connect to the PGSQL Server
-        conn = psycopg2.connect(database="assignment3", user="assignment3", host="localhost", password="assignment3")
-        cur = conn.cursor()
-        
-        cur.execute("""SELECT "id", "address" FROM "Sailor" WHERE "level" = 200 """)
-        a = cur.fetchall()
-        for x in a:
-            sys.stderr.write(str(x))
-        #close comm with the PGSQL Server
-        cur.close()
-        #commit the changes
-        #conn.commit()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print("shit")
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-    
-#?Step 8
-#?Step 9
-#Step 10 done
+
+    cur.execute(
+        """SELECT "id", "address" FROM "Sailor" WHERE "level" = 200 """)
+    a = cur.fetchall()
+    for x in a:
+        sys.stderr.write(str(x))
+
+# Step 8
+
+
+@measure(8)
+def create_btree_index(cur):
+    """creates btree index on the attribute "level" """
+
+    cur.execute("""CREATE INDEX level_idx ON "Sailor"(level); """)
+
+
+# Step 9 done
+
+
+@measure(9)
+def id_sailor_stderr9(cur):
+    """retrieves from the database and prints to stderr all the id of the 1M Sailors """
+
+    cur.execute("""SELECT "id" FROM "Sailor" """)
+
+    a = cur.fetchall()
+    for x in a:
+        print(x[0], file=sys.stderr)
+
+
+# Step 10 done
+
+
 @measure(10)
-def update_tuples_200():
+def update_tuples_200(cur):
     """Updates tuples where level = 200 ==> level = 210 """
-    conn = None
-    try:  
-        #connect to the PGSQL Server
-        conn = psycopg2.connect(database="assignment3", user="assignment3", host="localhost", password="assignment3")
-        cur = conn.cursor()
-        
-        cur.execute("""UPDATE "Sailor" SET "level" = 210 WHERE "level" = 200 """)
-        
-        #close comm with the PGSQL Server
-        cur.close()
-        #commit the changes
-        #conn.commit()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print("shit")
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-#Step 11 done
+
+    cur.execute(
+        """UPDATE "Sailor" SET "level" = 210 WHERE "level" = 200 """)
+
+# Step 11 done
+
+
 @measure(11)
-def print_tuples_210():
+def print_tuples_210(cur):
     """selects from the table sailor and prints to stderr the id and address fo the sailors with level = 210 """
-    conn = None
-    try:  
-        #connect to the PGSQL Server
-        conn = psycopg2.connect(database="assignment3", user="assignment3", host="localhost", password="assignment3")
-        cur = conn.cursor()
-        
-        cur.execute("""SELECT "id", "address" FROM "Sailor" WHERE "level" = 210 """)
-        a = cur.fetchall()
-        for x in a:
-            sys.stderr.write(str(x))
-        #close comm with the PGSQL Server
-        cur.close()
-        #commit the changes
-        #conn.commit()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print("shit")
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+
+    cur.execute(
+        """SELECT "id", "address" FROM "Sailor" WHERE "level" = 210 """)
+    a = cur.fetchall()
+    for x in a:
+        sys.stderr.write(str(x))
+
 
 if __name__ == '__main__':
-    #connect()
-    drop_tables()
-    create_tables()
-    insert_tuple_Sailor()
-    #prova() #?qui per debugging
-    insert_tuple_Boat()
-    #id_sailor_stderr()
-    update_tuples_185()
-    print_tuples_200()
-    update_tuples_200()
-    print_tuples_210()
+    dati_connessoine = connect()
 
-    #dovrebbe funzionare, poi si sposta l'output di step 5 qua
-    #sys.stderr.write("ciao")
+    conn = dati_connessoine[0]
+    cur = dati_connessoine[1]
 
+    drop_tables(cur)
+    create_tables(cur)
+    insert_tuple_Sailor(cur)
+    insert_tuple_Boat(cur)
+    id_sailor_stderr5(cur)
+    update_tuples_185(cur)
+    print_tuples_200(cur)
+    create_btree_index(cur)
+    id_sailor_stderr9(cur)
+    update_tuples_200(cur)
+    print_tuples_210(cur)
+
+    cur.close()
+    conn.close()
